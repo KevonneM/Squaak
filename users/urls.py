48e85c59083d638250ffb651a8e07a474ee07c73
MichaelB <michaelleroybrown98@gmail.com>
2022-02-08 14:28:00 -0700

    Added passwordreset

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from .views import SignUpView
+from django.contrib.auth import views as auth_views
+from . import views
 
-app_name = 'users'
 
 urlpatterns = [
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    #Password reset
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name= "reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_comlete"),
 ]
