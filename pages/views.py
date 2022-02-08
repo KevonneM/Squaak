29e72c3f99c1c1f3320cc6fc3067204dd63b035f
KevonneM <Kevonnem@gmail.com>
2022-02-08 12:29:41 -0700

    Added simple views for links

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,22 @@ from django .views.generic import TemplateView
 
 # Create your views here.
 
-# HomePage View
+# HomePage View.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+# View for the Profile page.
+class ProfilePageView(TemplateView):
+    template_name = 'profile.html'
+
+# View for the Friends page.
+class FriendsPageView(TemplateView):
+    template_name = 'friends.html'
+
+# View for the Messaging page.
+class MessagePageView(TemplateView):
+    template_name = 'message.html'
+
+# View for the Video chat page.
+class VideoChatView(TemplateView):
+    template_name = 'videochat.html'
