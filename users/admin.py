5f8f7649418fc4c14a17c8ec8d27d27094ac19bf
KevonneM <Kevonnem@gmail.com>
2022-02-18 04:33:24 -0700

    Used signals to create profile for each new user.

--- users/admin.py
+++ users/admin.py
@@ -1,16 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser, Profile
+from .models import Profile
 
-# Register your models here.
-
-class CustomUserAdmin(UserAdmin):
-
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ['email', 'username', 'age', 'is_staff',]
-
-admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Profile)
--- users/models.py
+++ users/models.py
@@ -1,21 +1,27 @@
-from re import A
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+# User model inheriting from Django's AbstractUser.
 class CustomUser(AbstractUser):
-
     age = models.PositiveIntegerField(null=True, blank=True)
 
-
 class Profile(models.Model):
-
-    REQUIRED_FIELDS = ('profile')
-
-    profile = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_images')
-    bio = models.TextField()
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f"{self.profile.username}'s Profile"
+        return f"{self.user.username}'s Profile"
+
+# User profile is created upon user signup.
+@receiver(post_save, sender=CustomUser)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+# User profile is saved upon user signup.
+@receiver(post_save, sender=CustomUser)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
