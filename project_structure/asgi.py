941d0e31b9c8274a018bbb050c48959747c1e162
KevonneM <Kevonnem@gmail.com>
2022-04-08 01:10:01 -0600

    Added Simple chat function.

--- project_structure/asgi.py
+++ project_structure/asgi.py
@@ -11,11 +11,19 @@ import os
 
 import django
 from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import messaging.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_structure.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
     "http": AsgiHandler(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            messaging.routing.websocket_urlpatterns
+        )
+    )
 })
