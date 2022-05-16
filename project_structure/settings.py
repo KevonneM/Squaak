4964382e665a49e7ddd606b309ab2b7adc016e20
KevonneM <Kevonnem@gmail.com>
2022-05-16 01:44:20 -0600

    Tweaked password reset, and hid some stuff.

--- project_structure/settings.py
+++ project_structure/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m(043ukp4o$qo63khel+p7(3)pogf35vgq%k9=__4_%nnm(wap'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -161,9 +163,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #SMTP Configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'Squaakvideochat@gmail.com'
-EMAIL_HOST_PASSWORD = "Mlb$419523"
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import SignUpView, CustomPasswordChangeView
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'users'
 
@@ -9,11 +10,12 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
     #Password reset
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name= "reset_password"),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name ='registration/password_reset.html',
+  email_template_name = 'registration/password_reset_email.html', success_url = reverse_lazy('users:password_reset_done')), name= "reset_password"),
 
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('users:login')),name="password_reset_confirm"),
     
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_comlete"),
     #password change
