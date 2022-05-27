34c908ffeb20c285079b5f6a79525fc0eba769d0
KevonneM <Kevonnem@gmail.com>
2022-05-26 18:59:22 -0600

    Styled search, friendspage, and some buttons.

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from django.contrib.auth.decorators import login_required
 
 from pages.views import SearchView
 from . import views
@@ -8,5 +9,5 @@ app_name = 'pages'
 urlpatterns = [
     path('', views.homepage_view, name='home'),
     path('my-profile/<int:pk>/', views.myprofilepageview, name='my_profile'),
-    path('user_search/', SearchView.as_view(), name='search'),
+    path('user_search/', login_required(SearchView.as_view()), name='search'),
 ]
