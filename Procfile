534ceb14871f8ff006dc0ab340ad129789820927
KevonneM <Kevonnem@gmail.com>
2022-05-31 22:30:00 -0600

    Switching to Daphne.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn project_structure.wsgi
+web: daphne project_structure.asgi:application --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runworker --settings=project_structure.settings -v2
--- project_structure/settings.py
+++ project_structure/settings.py
@@ -89,7 +89,7 @@ CHANNEL_LAYERS = {
     'default':{
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', '6379')],
+            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
